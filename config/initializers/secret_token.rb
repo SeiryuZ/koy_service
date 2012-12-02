e6c4f1b950b99069fc24e6cc0dd9267d77dcf861
Steven Only <SeiryuZ@hotmail.com>
2012-12-02 17:22:16 +0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KoyService::Application.config.secret_token = 'd241bee04c375cf8d5444c806e9b1a16c63e481278f0f1afc7b069cffe3a53f28fe45743f2017927ad6fed3542b0a20c007983e4807ddbe7af9e09f16c3df80c'
