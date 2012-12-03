53d3dca34ee2a7882cc6cf0a98f3c8cca4c75fe7
Steven Only <SeiryuZ@hotmail.com>
2012-12-03 21:04:48 +0700

    Deleted view and helper specs, Added User Model Testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,29 @@ class User < ActiveRecord::Base
   include BCrypt
   attr_accessible  :username
 
+  validates :username, presence: true, uniqueness: true
+  validates :password_hash, presence:true
+
   def password
     @password ||= Password.new(password_hash)
   end
 
   def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_hash = @password
+    if not new_password.blank?
+      @password = Password.create(new_password)
+      self.password_hash = @password
+    else
+      self.password_hash = nil
+    end
+  end
+
+
+  def is_admin?
+    self.admin
+  end
+  
+  def set_admin=(admin)
+    self.admin = admin
   end
 
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,76 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before do
+    @user = User.new()
+    @user.username = "admin"
+    @user.password = "passwordadmin"
+    @user.save
+  end
+
+  subject { @user }
+
+
+  describe "When initialized" do
+    it "should respond to desired methods" do
+
+      should respond_to(:username) 
+      should respond_to(:password) 
+      should respond_to(:set_admin=) 
+      should respond_to(:is_admin?)
+
+    end
+
+    it "should not respond to undesired methods" do 
+
+    end
+
+
+    it "should be valid " do
+      should be_valid
+    end
+
+  end
+
+  describe "When Username is not present" do 
+    before { @user.username = " " }
+    it { should_not be_valid }
+  end
+
+  describe "Password is not present" do
+    before { @user.password = " " }
+    it { should_not be_valid }
+  end
+
+
+  describe "When Username is already taken" do
+    before do 
+      @user = User.new()
+      @user.username = "admin"
+      @user.password = "passwordadmin"
+      @user2 = User.new() 
+      @user2.username = "admin"
+    end
+
+    it { should_not be_valid }
+  end
+
+
+  describe "When user admin status is changed" do
+
+    it "should change the status correctly" do
+      @user.set_admin = true
+      @user.is_admin?.should == true
+      @user.is_admin?.should_not == false
+
+      @user.set_admin = false
+      @user.is_admin?.should == false
+      @user.is_admin?.should_not == true
+    end
+
+  end
+
+
+
 end
